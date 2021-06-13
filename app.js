f0ff32b2e8a777b0264093d01a0765e59e364814
qian-27 <83451817+qian-27@users.noreply.github.com>
2021-06-13 23:11:51 +0300

    First EJS Templates
    app.js
    views/list.ejs

--- app.js
+++ app.js
@@ -5,10 +5,47 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
-app.get("/", function(req, res){
-  res.send("Hello");
+app.set('view engine', 'ejs');
+
+app.get("/", function(req, res) {
+  var today = new Date();
+  var currentDay = today.getDay();
+  var day = "";
+
+  switch (currentDay) {
+    case 0:
+      day = "Sunday";
+      break;
+    case 1:
+      day = "Monday";
+      break;
+    case 2:
+      day = "Tuesday";
+      break;
+    case 3:
+      day = "Wednesday";
+      break;
+    case 4:
+      day = "Thursday";
+      break;
+    case 5:
+      day = "Friday";
+      break;
+    case 6:
+      day = "Saturday";
+      break;
+    default:
+      console.log("Error: current day is equal to: " + currentDay);
+  }
+
+  res.render("list", {
+    kindOfDay: day
+  });
+  
 });
 
-app.listen(3000, function(){
-  console.log("Server started on port 3000.");
+
+
+app.listen(4000, function() {
+  console.log("Server started on port 4000.");
 });
